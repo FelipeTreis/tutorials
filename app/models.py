9728740598484ff37236a15add6e3e1b137e4a63
Felipe Treis <felipetreis1@gmail.com>
2022-11-05 15:00:27 -0300

    Created Content model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Content(models.Model):
+    title = models.CharField(max_length=254, null=False, blank=False)
+    text = models.TextField(null=False, blank=False)
+    slug = models.SlugField(unique=True, null=False, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
