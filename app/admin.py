cecb25003418d989fe42f2cc168d3247a99e4cf9
Felipe Treis <felipetreis1@gmail.com>
2022-11-05 15:09:51 -0300

    Created admin class to Content

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Content
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'is_published', 'title', 'author', )
+    list_display_links = ('title', )
+    list_editable = ('is_published', )
+    list_filter = ('title', )
+    list_per_page = 10
+    prepopulated_fields = {"slug": ('title', )}
